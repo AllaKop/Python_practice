bbb5485a4334652196aa1a632d2eab7d7b704a69
allakopiichenko <Positivism02>
2022-05-11 19:58:19 +0300

    The tasks for if...elif...else topic

---
+++ main.py
@@ -0,0 +1,18 @@
+
+# Practice tasks from https://www.programiz.com/python-programming/for-loop webinars.
+
+score = int(input('Enter your score '))
+if score >= 50:
+    print('You have passed your exam.\n Congratulations')
+else:
+    print('Sorry, you have failed your test')
+
+# Task 2
+
+number = float(input('Enter your number '))
+if number > 0:
+    print('The number is positive')
+elif number == 0:
+    print('The number is Zero')
+else:
+    print('The number is negative')
