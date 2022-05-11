e4a66bee203701c8d8cf5286a6a03c852db0e470
allakopiichenko <Positivism02>
2022-05-11 20:49:14 +0300

    The tasks for "for loop"

---
+++ Loop.py
@@ -0,0 +1,29 @@
+# Task 1
+
+text = 'Python'
+for character in text:
+    print(character)
+
+# Task 2. For loop for a list
+
+language = ['English', 'Ukrainian', 'French']
+for language in language:
+    print(language)
+
+# Task 3.
+count: int = 1
+while count <= 10:
+    print(count)
+    count += 1
+
+# Task 4.
+for count in range(1, 6):
+    print(count)
+
+# Task 5. Find the sum of number from 1 to 100.
+sum_of_numbers = 0
+for i in range(1, 101):
+    sum_of_numbers = sum_of_numbers + i
+print(sum_of_numbers)
+
+
