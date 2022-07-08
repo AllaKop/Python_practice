645bc872f5c65a5b2158e0dd8b227d58ebd0e9b1
allakopiichenko <Positivism02>
2022-07-08 21:36:13 +0300

    Changed Dictionaries.py, Set.py

--- Python_Datatypes/Dictionaries.py
+++ Python_Datatypes/Dictionaries.py
@@ -1,4 +1,5 @@
 # Dictionary is unordered sequence of items with a key.
+# Keys have to be of immutable type and unique.
 
 #dictionaries comprehension
 
@@ -15,3 +16,49 @@ old_price = {'milk': 1.02, 'coffee': 2.5, 'bread': 2.5}
 
 new_price = {key: value * 1.5 if value > 2 else value for (key, value) in old_price.items()}
 print(new_price)
+
+# Creating of a dictionary
+person = {'Name': 'Linux', 'Age': 21}
+print(person)
+print(person['Name'])
+print(person.get('Hobbies'))
+
+# Change value
+
+person['Name'] = 'Denise'
+print(person)
+
+# Adding items
+
+person['Hobby'] = 'Dancing'
+print(person)
+
+# For loop
+
+for key in person:
+    print(key)
+
+# We can remove a particular item in a dictionary by using the pop() method.
+# This method removes an item with the provided key and returns the value.
+
+print(person.pop('Hobby'))
+print(person)
+
+# Dictionary Comprehension
+
+squares = {x: x*x for x in range(6)}
+print(squares)
+
+# Dictionary Comprehension with if conditional
+
+odd_squares = {x: x*x for x in range(11) if x % 2 == 1}
+print(odd_squares)
+
+# Iterating through a Dictionary
+
+squares = {1: 1, 3: 9, 5: 25, 7: 49, 9: 81}
+for i in squares:
+    print(squares[i])
+
+
+
--- Python_Datatypes/Set.py
+++ Python_Datatypes/Set.py
@@ -1,4 +1,5 @@
 # Set is unordered sequence of unique items.
+# Set is mutable, but can contain only immutable items like Strings, Tuples, Numbers.
 
 #set comprehension
 
@@ -6,3 +7,77 @@ word = 'programing'
 
 alphabets = {x for x in word}
 print(alphabets)
+
+# Creating a set
+
+animal = {'Tiger', 'Panther', 'Lion'}
+print(animal)
+
+# Add element to the set
+animal.add('Monkey')
+print(animal)
+
+# Update one set with another one
+
+pets = {'Cat', 'Dog'}
+animal.update(pets)
+print(animal)
+
+# Removing item from the set
+# The only difference between the two is that the discard() function leaves a set unchanged
+# if the element is not present in the set.
+# On the other hand, the remove() function will raise an error
+# in such a condition (if element is not present in the set).
+
+animal.remove('Dog')
+animal.discard('Monkey')
+print(animal)
+
+# Removing all items
+
+animal.clear()
+print(animal)
+
+# Union function
+
+domestic_animals = {'Dog', 'Cat', 'Parrot'}
+wild_animals = {'Lion', 'Tiger', 'Parrot'}
+animals = domestic_animals.union(wild_animals)
+print(animals)
+
+# Intersection
+
+domestic_animals1 = {'Dog', 'Cat', 'Parrot'}
+wild_animals1 = {'Lion', 'Tiger', 'Parrot'}
+animals1 = domestic_animals1.intersection(wild_animals1)
+print(animals1)
+
+# Difference
+
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+print(A.difference(B))
+
+# Set Symmetric Difference
+
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+print(A.symmetric_difference(B))
+
+# Frozenset is a new class that has the characteristics of a set, but its elements cannot be changed once assigned.
+# While tuples are immutable lists, frozensets are immutable sets.
+# Sets being mutable are unhashable, so they can't be used as dictionary keys.
+# On the other hand, frozensets are hashable and can be used as keys to a dictionary.
+
+A1 = frozenset([1, 2, 3, 4])
+B2 = frozenset([3, 4, 5, 6])
+print(type(A1))
+
+
+
+
+
+
+
+
+
