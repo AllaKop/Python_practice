d786f706718af654a7c3a6975434b069709b2bca
allakopiichenko <Positivism02>
2022-07-26 19:52:08 +0300

    Added directory Python_Advance: - Python_closure.py

---
+++ Addvance_topics/Python_closure.py
@@ -0,0 +1,37 @@
+# Nested function
+
+def print_msg(msg):
+    # This is the outer enclosing function
+
+    def printer():
+        # This is the nested function
+        print(msg)
+
+    printer()
+
+
+# We execute the function
+# Output: Hello
+
+print_msg("Hello")
+
+
+def print_msg(msg):
+    # This is the outer enclosing function
+
+    def printer():
+        # This is the nested function
+        print(msg)
+
+    return printer  # returns the nested function
+
+
+# Now let's try calling this function.
+# Output: Hello
+another = print_msg("Hello")
+another()
+
+del print_msg
+another()
+
+# print_msg("Hello")
